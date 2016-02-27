718d00038a857042ed20a198c844aadf63e4733f
Christian Hergert <chergert@redhat.com>
2016-02-27 14:14:39 -0800

    panel: expand PnlDockWidget children during multi-paned resize

--- src/pnl-panel.c
+++ src/pnl-panel.c
@@ -17,6 +17,7 @@
  */
 
 #include "pnl-panel.h"
+#include "pnl-dock-widget.h"
 
 typedef struct
 {
@@ -25,9 +26,40 @@ typedef struct
 G_DEFINE_TYPE (PnlPanel, pnl_panel, PNL_TYPE_MULTI_PANED)
 
 static void
+pnl_panel_resize_drag_begin (PnlMultiPaned *multi_paned,
+                             GtkWidget     *child)
+{
+  G_GNUC_UNUSED PnlPanel *self = (PnlPanel *)multi_paned;
+
+  g_assert (PNL_IS_PANEL (self));
+  g_assert (GTK_IS_WIDGET (child));
+
+  if (PNL_IS_DOCK_WIDGET (child))
+    pnl_dock_widget_set_reveal_child (PNL_DOCK_WIDGET (child), TRUE);
+
+  PNL_MULTI_PANED_CLASS (pnl_panel_parent_class)->resize_drag_end (multi_paned, child);
+}
+
+static void
+pnl_panel_resize_drag_end (PnlMultiPaned *multi_paned,
+                           GtkWidget     *child)
+{
+  G_GNUC_UNUSED PnlPanel *self = (PnlPanel *)multi_paned;
+
+  g_assert (PNL_IS_PANEL (self));
+  g_assert (GTK_IS_WIDGET (child));
+
+  PNL_MULTI_PANED_CLASS (pnl_panel_parent_class)->resize_drag_begin (multi_paned, child);
+}
+
+static void
 pnl_panel_class_init (PnlPanelClass *klass)
 {
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+  PnlMultiPanedClass *multi_paned_class = PNL_MULTI_PANED_CLASS (klass);
+
+  multi_paned_class->resize_drag_begin = pnl_panel_resize_drag_begin;
+  multi_paned_class->resize_drag_end = pnl_panel_resize_drag_end;
 
   gtk_widget_class_set_css_name (widget_class, "panel");
 }
